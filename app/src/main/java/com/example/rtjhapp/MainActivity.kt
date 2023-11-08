d3e047faa5805161731e04514517338ac7f80531
吴珺哲 <zhejunwu97@163.com>
2023-11-08 17:21:40 +0800

    初始化ui图片文件

--- app/src/main/java/com/example/rtjhapp/MainActivity.kt
+++ app/src/main/java/com/example/rtjhapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.rtjhapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rtjhapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        binding!!.tvTest.text = "你好呀"
     }
 }
