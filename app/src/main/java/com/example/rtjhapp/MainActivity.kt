e4215f48c1a4473a80e6f378bd7c0cb8fea79153
wujunzhe <zhejunwu97@163.com>
2023-11-09 16:42:37 +0800

    顶部时间模块ui完成

--- app/src/main/java/com/example/rtjhapp/MainActivity.kt
+++ app/src/main/java/com/example/rtjhapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.rtjhapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.example.rtjhapp.databinding.ActivityMainBinding
 
  class MainActivity : AppCompatActivity() {
@@ -10,7 +13,9 @@ import com.example.rtjhapp.databinding.ActivityMainBinding
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
-
         binding!!.tvTest.text = "你好呀"
+        // 在 Activity 的 onCreate 方法中添加以下代码
+        val controller = window.insetsController
+        controller?.hide(WindowInsets.Type.statusBars())
     }
 }
