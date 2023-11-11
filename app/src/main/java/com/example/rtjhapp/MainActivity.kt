833efa0dc44f9559dd2bf4b497063ba7a90bf731
wujunzhe <zhejunwu97@163.com>
2023-11-11 16:55:23 +0800

    完成音乐模块的ui设计 完成电话模块ui设计

--- app/src/main/java/com/example/rtjhapp/MainActivity.kt
+++ app/src/main/java/com/example/rtjhapp/MainActivity.kt
@@ -10,8 +10,8 @@ import com.example.rtjhapp.databinding.ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding!!.vAirCondition.tempCircle.setValue("30",60f)
-        binding!!.vAirCondition.humidityCircle.setValue("50",80f)
+        binding!!.vAirCondition.tempCircle.setValue("30",36f)
+        binding!!.vAirCondition.humidityCircle.setValue("50",70f)
         setContentView(binding!!.root)
 
         // 在 Activity 的 onCreate 方法中添加以下代码
---
+++ app/src/main/java/com/example/rtjhapp/view/CallView.kt
@@ -0,0 +1,9 @@
+package com.example.rtjhapp.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class CallView @JvmOverloads constructor(context : Context,attrs: AttributeSet? = null, defStyleAttr:Int = 0): FrameLayout(context,attrs,defStyleAttr) {
+
+}
